5913b31f55f8da0eb2a2fe75380e52e9cc97e970
Valentin PRUGNAUD <valentin@whatdafox.com>
2014-09-17 02:08:12 -0700

    French localization

---
+++ src/lang/fr/units.php
@@ -0,0 +1,11 @@
+<?php
+
+return array(
+    "second"    => "seconde|secondes",
+    "minute"    => "minute|minutes",
+    "hour"      => "heure|heures",
+    "day"       => "jour|jours",
+    "week"      => "semaine|semaines",
+    "month"     => "mois|mois",
+    "year"      => "an|ans",
+);
