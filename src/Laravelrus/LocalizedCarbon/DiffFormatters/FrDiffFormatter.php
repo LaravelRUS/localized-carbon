dcb48ae8c3a24d20c010a958f51baf2b8afb25d5
Valentin PRUGNAUD <valentin@sccnorthwest.com>
2014-09-17 15:48:38 -0700

    diffForHumans french translation

---
+++ src/Laravelrus/LocalizedCarbon/DiffFormatters/FrDiffFormatter.php
@@ -0,0 +1,22 @@
+<?php namespace Laravelrus\LocalizedCarbon\DiffFormatters;
+
+
+class FrDiffFormatter implements DiffFormatterInterface {
+
+    public function format($isNow, $isFuture, $delta, $unit) {
+
+        $unitStr = \Lang::choice("localized-carbon::units." . $unit, $delta, array(), 'fr');
+
+        $txt = $delta . ' ' . $unitStr;
+
+        var_dump($isNow, $isFuture);
+
+        if ($isNow) {
+            $when = ($isFuture) ?  'Dans ' : 'Il y a ';
+            return $when . $txt;
+        }
+
+        return $txt .= ($isFuture) ? ' après' : ' avant';
+
+    }
+}
