6f778bdad248b5c50dfb158d3681ee4e3c9ed0eb
Niels Faurskov <jazerixthegreat@gmail.com>
2015-02-28 00:43:24 +0100

    Danish language added and english units

---
+++ src/lang/da/months.php
@@ -0,0 +1,16 @@
+<?php
+
+return array(
+    "january" => "Januar",
+    "february" => "Febryar",
+    "march" => "Marts",
+    "april" => "April",
+    "may" => "Maj",
+    "june" => "Juni",
+    "july" => "Juli",
+    "august" => "August",
+    "september" => "September",
+    "october" => "Oktober",
+    "november" => "November",
+    "december" => "December",
+);
