846d7c4b4fce3e56f70d3b1d08cd0a7f00b0885f
Rodrigo Troncoso <rod.tronco@gmail.com>
2015-04-09 11:19:41 -0300

    Added full genitive support for Spanish language months

---
+++ src/lang/es/months.php
@@ -0,0 +1,16 @@
+<?php
+
+return array(
+    "january" => "Enero",
+    "february" => "Febrero",
+    "march" => "Marzo",
+    "april" => "Abril",
+    "may" => "Mayo",
+    "june" => "Junio",
+    "july" => "Julio",
+    "august" => "Agosto",
+    "september" => "Septiembre",
+    "october" => "Octubre",
+    "november" => "Noviembre",
+    "december" => "Diciembre",
+);
