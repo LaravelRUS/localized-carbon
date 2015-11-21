cd117652696c589e97ed4a28303a963d6f80fa52
Markus Tenghamn <markus@tenghamn.com>
2015-11-21 23:46:00 +0100

    Added support for Swedish Language
    Added support for Swedish Language

---
+++ src/lang/sv/units.php
@@ -0,0 +1,11 @@
+<?php
+
+return array(
+    "second"    => "sekund|sekunder",
+    "minute"    => "minut|minuter",
+    "hour"      => "timme|timmar",
+    "day"       => "dag|dagar",
+    "week"      => "vecka|veckor",
+    "month"     => "månad|månader",
+    "year"      => "år",
+);
