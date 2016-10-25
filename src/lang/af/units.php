ed0fc46ceec431ffc40afc5f57a76011d8268ed2
Ettienne Louw <ettienne.louwki@gmail.com>
2016-10-25 11:52:36 +0200

    Create units.php
    added afrikaans units file

---
+++ src/lang/af/units.php
@@ -0,0 +1,10 @@
+<?php
+return array(
+    "second"    => "sekonde|sekondes",
+    "minute"    => "minuut|minute",
+    "hour"      => "uur|ure",
+    "day"       => "dag|dae",
+    "week"      => "week|weke",
+    "month"     => "maand|maande",
+    "year"      => "jaar|jare",
+);
