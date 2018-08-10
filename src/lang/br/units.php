2e720f6987d36b4691b2c052bb8ed22a7e989c71
Rafhael Marsigli <rafhael@laranja.info>
2018-08-10 05:27:08 -0300

    Brazilian Portuguese lang files

---
+++ src/lang/br/units.php
@@ -0,0 +1,11 @@
+<?php
+
+return array(
+    "second"    => "segundo|segundos",
+    "minute"    => "minuto|minutos",
+    "hour"      => "hora|horas",
+    "day"       => "dia|dias",
+    "week"      => "semana|semanas",
+    "month"     => "mês|meses",
+    "year"      => "ano|anos",
+);
